c2ef58df69e31f5a0c403ffeae008e1e5913d8c5
bennymeade <brendan_meade@yahoo.co.uk>
2022-11-23 20:29:01 +0000

    chore: add ci workflow

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8888/",
+    baseUrl: "http://127.0.0.1:3000/",
     supportFile: false,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    experimentalWebKitSupport: true,
+    video: false,
   },
 });
